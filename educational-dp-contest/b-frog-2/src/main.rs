197ecc987634e8bb3f4e6fe6671021a3a7c52ec3
takayuki murata <muratata@scala-com.jp>
2026-05-10 15:04:50 +0900

    Add comment

--- educational-dp-contest/b-frog-2/src/main.rs
+++ educational-dp-contest/b-frog-2/src/main.rs
@@ -1,30 +1,33 @@
 fn resolve(hs: &[usize], k: usize) -> usize {
     let len = hs.len();
-
     let k = k.min(len);
 
     let mut prevs = vec![0; k];
+    // prevs[0]: 1つ前からの遷移
+    // prevs[1]: 2つ前からの遷移
+    // ...
 
+    // 初期化
+    // index k - 1 の足場まで到達した状態を作る
+    // prevs[0]: k-1からkへの遷移
+    // prevs[1]: k-2からkへの遷移
+    // ...
+    // kの状態ではk個前からの遷移はないのでk-1までで良い
     for i in 0..(k - 1) {
         prevs[i] = hs[k - 1 - i].abs_diff(hs[0]);
     }
 
     for i in k..len {
-        // let mut cur = prevs[0] + hs[i].abs_diff(hs[i - k]);
         let mut cur = prevs[0] + hs[i].abs_diff(hs[i - 1]);
-        for k in 1..k {
-            let tmp = prevs[k] + hs[i].abs_diff(hs[i - k - 1]);
-            if cur > tmp {
-                cur = tmp;
-            }
+        for j in 1..k {
+            cur = cur.min(prevs[j] + hs[i].abs_diff(hs[i - j - 1]));
         }
-
+        // 次の状態へ進めるために一つずつずらす
         for j in (1..k).rev() {
             prevs[j] = prevs[j - 1];
         }
         prevs[0] = cur;
     }
-
     prevs[0]
 }
 
