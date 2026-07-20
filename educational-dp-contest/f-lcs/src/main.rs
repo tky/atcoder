98c7c7e0b7c42ad800c7ba5da2ce9c3a27d3cd3c
takayuki murata <muratata@scala-com.jp>
2026-07-20 18:41:45 +0900

    Update comments

--- educational-dp-contest/f-lcs/src/main.rs
+++ educational-dp-contest/f-lcs/src/main.rs
@@ -1,27 +1,19 @@
-fn chmax<T: PartialOrd + Copy>(a: &mut T, b: T) {
-    if *a < b {
-        *a = b;
-    }
-}
-
 fn lcs(s: &str, t: &str) -> String {
     let ss = s.chars().collect::<Vec<char>>();
     let ts = t.chars().collect::<Vec<char>>();
 
-    let mut dp = vec![vec![0; ts.len() + 1]; ss.len() + 1];
+    let mut dp = vec![vec![0; t.len() + 1]; s.len() + 1];
 
-    for i in 1..=ss.len() {
-        for j in 1..=ts.len() {
+    for i in 1..=s.len() {
+        for j in 1..=t.len() {
             // S[i] = T[j]なのでLCSを伸ばせる
             if ss[i - 1] == ts[j - 1] {
                 dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                // S[i]を使わない場合
+                // S[1..i-1]とT[1..j]、あるいはS[1..i]とT[1..j-1]の問題となる
+                dp[i][j] = dp[i - 1][j].max(dp[i][j - 1]);
             }
-            // S[i]を使わない場合
-            // S[1..i-1]とT[1..j]の問題となり、dp[i-1][j]が答えとなる
-            dp[i][j] = dp[i][j].max(dp[i - 1][j]);
-            // T[j]を使わない場合
-            // S[1..i]とT[1..j-1]の問題となる
-            dp[i][j] = dp[i][j].max(dp[i][j - 1]);
         }
     }
 
@@ -36,14 +28,14 @@ fn lcs(s: &str, t: &str) -> String {
             out.push(ss[i - 1]);
             i -= 1;
             j -= 1;
-
-        // dp[i-1][j]はss[i-1]を捨てた場合の最適値
-        // dp[i][j-1]はts[j-1]を捨てた場合の最適値
-        // dp[i-1][j] > dp[i][j-1]ということはss[i-1]を捨てたほうが長いLCSをつくれる
-        // ss[i-1] を捨てる、つまり i を 1 減らす
+        // dp[i-1][j] は、ss[i-1] を使わない場合の LCS 長
+        // dp[i][j-1] は、ts[j-1] を使わない場合の LCS 長
+        //
+        // dp[i-1][j] > dp[i][j-1] なので、
+        // ss[i-1] を使わない状態へ戻る方が長い LCS を保てる。
+        // よって ss[i-1] を捨てて i を 1 減らす。
         } else if dp[i - 1][j] > dp[i][j - 1] {
             i -= 1;
-        // それ以外のケースではts[j-1]を捨てる
         } else {
             j -= 1;
         }
