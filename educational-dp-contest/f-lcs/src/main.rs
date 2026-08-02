315229cc2a6d74478da223c80244fc5e15d230d6
takayuki murata <muratata@scala-com.jp>
2026-08-02 20:37:46 +0900

    Fix logic and add comments

--- educational-dp-contest/f-lcs/src/main.rs
+++ educational-dp-contest/f-lcs/src/main.rs
@@ -2,10 +2,12 @@ fn lcs(s: &str, t: &str) -> String {
     let ss = s.chars().collect::<Vec<char>>();
     let ts = t.chars().collect::<Vec<char>>();
 
-    let mut dp = vec![vec![0; t.len() + 1]; s.len() + 1];
+    // s.len() / t.len() よりもss.len() / ts.len()を使う方が安全
+    // この問題は英小文字なので一致するが一般のUTF-8では一致しないことがある
+    let mut dp = vec![vec![0; ts.len() + 1]; ss.len() + 1];
 
-    for i in 1..s.len() {
-        for j in 1..t.len() {
+    for i in 1..=ss.len() {
+        for j in 1..=ts.len() {
             if ss[i - 1] == ts[j - 1] {
                 dp[i][j] = dp[i - 1][j - 1] + 1;
             } else {
@@ -14,8 +16,8 @@ fn lcs(s: &str, t: &str) -> String {
         }
     }
 
-    let mut i = s.len();
-    let mut j = t.len();
+    let mut i = ss.len();
+    let mut j = ts.len();
 
     let mut ans = Vec::new();
 
@@ -24,16 +26,18 @@ fn lcs(s: &str, t: &str) -> String {
             ans.push(ss[i - 1]);
             i -= 1;
             j -= 1;
+        // iを減らした方がdp(LCSの長さ)が大きくなるならiを減らす
+        } else if dp[i - 1][j] > dp[i][j - 1] {
+            i -= 1;
+        // そうでなければ、dp[i][j-1] 側へ戻る
+        // 同値の場合はどちらへ戻ってもよい
         } else {
-            if dp[i - 1][j] > dp[i][j - 1] {
-                i -= 1;
-            } else {
-                j -= 1;
-            }
+            j -= 1;
         }
     }
 
     ans.reverse();
+
     ans.iter().collect()
 }
 
@@ -45,6 +49,7 @@ mod tests {
     fn test_lcs_length() {
         assert_eq!(lcs("axyb", "abyxb"), "ayb");
         assert_eq!(lcs("aa", "xayaz"), "aa");
+        assert_eq!(lcs("abc", "ab"), "ab");
     }
 }
 fn main() {
