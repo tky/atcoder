cce9da4eaf701f9f87432863f8c5c402dc9250e0
takayuki murata <muratata@scala-com.jp>
2026-09-04 17:20:11 +0900

    resolve abc212 c

--- binary_search/src/bin/abc212_c_min_difference.rs
+++ binary_search/src/bin/abc212_c_min_difference.rs
@@ -4,7 +4,46 @@
 // N, M <= 2*10^5 なので総当たり O(NM) は不可
 // ヒント: 片方をソートすると「aに一番近いbの候補」は2つに絞れる
 fn resolve(a: &[usize], b: &[usize]) -> usize {
-    todo!()
+    let mut a = a.to_vec();
+    let mut b = b.to_vec();
+    a.sort_unstable();
+    b.sort_unstable();
+
+    let mut ans = usize::MAX;
+
+    for i in 0..a.len() {
+        let ai = a[i];
+        let pos = binary_search(&b, ai);
+        if pos < b.len() {
+            ans = ans.min(ai.abs_diff(b[pos]));
+        }
+    }
+
+    for i in 0..b.len() {
+        let bi = b[i];
+        let pos = binary_search(&a, bi);
+        if pos < a.len() {
+            ans = ans.min(bi.abs_diff(a[pos]));
+        }
+    }
+
+    ans
+}
+
+fn binary_search(vs: &[usize], v: usize) -> usize {
+    let mut left = 0;
+    let mut right = vs.len();
+
+    while left < right {
+        let mid = (left + right) / 2;
+        if vs[mid] < v {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+
+    left
 }
 
 fn main() {
@@ -28,10 +67,7 @@ mod tests {
     #[test]
     fn sample_03() {
         assert_eq!(
-            resolve(
-                &[82, 76, 82, 82, 71, 70],
-                &[17, 39, 67, 2, 45, 35, 22, 24]
-            ),
+            resolve(&[82, 76, 82, 82, 71, 70], &[17, 39, 67, 2, 45, 35, 22, 24]),
             3
         );
     }
